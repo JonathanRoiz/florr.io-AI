9e3dec775a3fb2cc87ac3d7fbde3fa54c2b0d011
JonathanRoiz <jonathanroiz202@gmail.com>
2024-10-18 23:31:17 +0000

    Label Data

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ from ultralytics import YOLO
 
 model = YOLO("best.pt")
 
-results = model("raw_images/Capture.PNG", conf=0.7)
+results = model("raw_images/Capture.PNG", conf=0.8)
 
 results[0].show()
