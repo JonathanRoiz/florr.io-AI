fc0e2d589bd8a634803be1ceebe0f3c3714216ff
JonathanRoiz <jonathanroiz202@gmail.com>
2024-10-18 21:31:07 +0000

    Add training data + train model

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ from ultralytics import YOLO
 
 model = YOLO("best.pt")
 
-results = model("raw_images/Capture.PNG")
+results = model("raw_images/Capture.PNG", conf=0.7)
 
 results[0].show()
--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov10m.yaml")
+model = YOLO("yolov10m.pt")
 
-model.train(data="data.yaml", epochs=100, imgsz=640, workers=0)
+model.train(data="data_custom.yaml", epochs=100, imgsz=640)
 
